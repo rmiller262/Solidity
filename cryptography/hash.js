296669db7a1ba01640544fdd6fc90e13a577d17b
rmiller262 <54479403+rmiller262@users.noreply.github.com>
2022-11-30 09:05:16 -0500

    Add files via upload

---
+++ cryptography/hash.js
@@ -0,0 +1,3 @@
+const sha256 = require('js-sha256'); // npm install js-sha256
+
+//Alice has a secret document she wants to sent. She does not want to reveal personal information and will use bblockchain to send a pre image hash
